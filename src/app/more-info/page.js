2ccdaa2040a926340ac74e9ac2419495b013fe2c
Theojohn Sosa <theojohnsosa@gmail.com>
2026-04-20 13:35:07 +0800

    feat: added motion to main section in (more-info) page.js

--- src/app/more-info/page.js
+++ src/app/more-info/page.js
@@ -1,10 +1,18 @@
+"use client"
+
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+
+import { motion } from 'motion/react'
 
 const page = () => {
   return (
     <>
-      <div className='flex flex-col md:flex-row'>
+      <motion.div
+        initial={{ opacity: 0, x: 15 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className='flex flex-col md:flex-row'>
         <div className='hidden md:block'>
           <Sidebar />
         </div>
@@ -73,7 +81,7 @@ const page = () => {
           </div>
         
         </main>
-      </div>
+      </motion.div>
     </>
   )
 }
