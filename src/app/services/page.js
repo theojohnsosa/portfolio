9348f6a5e2d6b1a210e55610544689c2c03354f1
Theojohn Sosa <theojohnsosa@gmail.com>
2026-04-20 13:33:31 +0800

    feat: added motion to (services) page.js

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,11 +1,19 @@
+"use client"
+
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Image from 'next/image'
 
+import { motion } from 'motion/react'
+
 const page = () => {
   return (
     <>
-      <div className='flex flex-col md:flex-row'> 
+      <motion.div
+        initial={{ opacity: 0, x: 15 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className='flex flex-col md:flex-row'> 
         <div className='hidden md:block'>
           <Sidebar />
         </div>
@@ -118,7 +126,7 @@ const page = () => {
           </div>
 
         </main>
-      </div>
+      </motion.div>
     </>
   )
 }
